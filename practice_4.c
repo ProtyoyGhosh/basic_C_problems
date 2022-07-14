2ac07cfb99386426e64280903922046989d3f5b6
protyoy kumar ghosh <pghosh0615@gmail.com>
2022-07-14 21:01:38 +0530

    RBG values to CMYK values

--- practice_4.c
+++ practice_4.c
@@ -1,7 +1,7 @@
 // check wheather the traingle is isoscales,equilateral,scalene,right-angled?
 // isoscale = two equal sides, equilateral = all equal sides,
 // scalene = none equal sides, right-angeled = (hy^2 = per^2 + base^2)
-#include <stdio.h>
+/*#include <stdio.h>
 int main(){
     int hypontenuse,perpendiculer,base;
     printf("\n Enter length of three sides of the traingle: ");
@@ -24,5 +24,48 @@ int main(){
         printf("\n This is right angled triangle because it has: a(sqr) = b(sqr) + c(sqr)");
     }
     
+    return 0;
+} */
+
+
+
+// RGB to CMYK color
+#include <stdio.h>
+int main(){
+    float red,green,blue,white,cyan,magenta,yellow,black,max;
+
+    printf("\n Enter the value of R,G,B in range of (0-255): ");
+    scanf("%f %f %f", &red,&green,&blue);
+
+    if (red == 0 && green == 0 && blue == 0)
+    {
+        cyan = magenta = yellow = 0;
+        black = 1;
+        black = 1;
+    }
+    else
+    {
+        red = red / 255;
+        green = green / 255;
+        blue = blue / 255;
+
+        max = red;
+        if (green > red)
+        {
+            max = green;
+        }
+        if (blue > red)
+        {
+            max = blue;
+        }
+        white = max;
+        
+        cyan = (white - red) / white ; 
+        magenta = (white - green) / white ; 
+        yellow = (white - blue) / white ; 
+        black = 1 - white;
+    }
+    
+    printf("CMYK : %f %f %f %f", cyan,magenta,yellow,black);
     return 0;
 }
