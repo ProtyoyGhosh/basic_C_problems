b97661ed69df8a5098adbc909bee2b4cfc2c9430
protyoy kumar ghosh <pghosh0615@gmail.com>
2022-08-09 09:42:27 +0530

    prime num between 1 to 300

--- do_w_loop.c
+++ do_w_loop.c
@@ -18,7 +18,7 @@ int main(){
     {
         printf("It is a prime number");
     }
-}*/
+}
 
 
 
@@ -38,4 +38,28 @@ int main(){
         scanf("%c",&another);
     } while (another == 'y');
     return 0;
+}*/
+
+
+
+// print all the prime number between 1 to 300
+#include <stdio.h>
+int main(){
+    int i,n;
+    printf("Prime numbers between 1 to 300 are : \n1\t");
+    for ( n = 1; n < 300; n++)
+    {
+        // i = 2;
+        for ( i = 2; i < n; i++)
+        {
+            if (n % i == 0)
+            break;
+        }
+        if (i == n)
+        {
+            printf("%d\t", n);
+        }
+       
+    }
+    return 0; 
 }
